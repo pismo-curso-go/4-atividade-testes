499657212d069c7862643fe88675502ea3d1d5e7
DiegoBarcellos <diegobarcellos.dev@gmail.com>
2025-06-02 12:03:00 -0300

    feat: create divide tests

--- diego-barcellos/calculator_test.go
+++ diego-barcellos/calculator_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"testing"
 )
 
@@ -51,6 +52,29 @@ func Test_Multiply(t *testing.T) {
 		result := Multiply(test.num1, test.num2)
 		if result != test.ExpResult {
 			t.Errorf("Multiply(%d, %d) = %d; want %d", test.num1, test.num2, result, test.ExpResult)
+		}
+	}
+}
+
+func Test_Divide(t *testing.T) {
+	tests := []Test{
+		{6, 2, 3, nil},
+		{10, 5, 2, nil},
+		{5, 0, 0, errors.New("invalid division by zero")},
+	}
+
+	for _, test := range tests {
+		result, err := Divide(test.num1, test.num2)
+		if err != nil && test.ExpError == nil {
+			t.Errorf("Divide(%d, %d) returned an error: %v; want no error", test.num1, test.num2, err)
+			continue
+		}
+		if err == nil && test.ExpError != nil {
+			t.Errorf("Divide(%d, %d) = %f; want error %v", test.num1, test.num2, result, test.ExpError)
+			continue
+		}
+		if result != float64(test.ExpResult) {
+			t.Errorf("Divide(%d, %d) = %f; want %d", test.num1, test.num2, result, test.ExpResult)
 		}
 	}
 }
